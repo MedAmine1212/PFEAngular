825bda0485c09b5fed4482530d082e341b1135c5
ReserveHotelOnline <dr.kira1@hotmail.com>
2019-12-01 20:15:39 +0100

    component Panier

--- src/app/FRONT-OFFICE/panier/panier.component.ts
+++ src/app/FRONT-OFFICE/panier/panier.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./panier.component.css']
 })
 export class PanierComponent implements OnInit {
+  private isMobile: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.isMobile = /msie\s|trident\/|edge\//i.test(window.navigator.userAgent);
   }
 
 }
