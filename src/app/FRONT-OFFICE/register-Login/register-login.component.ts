5d6495cba34992e0663f53c12ec0213343268d5d
ReserveHotelOnline <dr.kira1@hotmail.com>
2019-12-08 00:53:57 +0100

    hethy l 5edma l final l feha kol chay

--- src/app/FRONT-OFFICE/register-Login/register-login.component.ts
+++ src/app/FRONT-OFFICE/register-Login/register-login.component.ts
@@ -9,10 +9,19 @@ import {AuthenticationService} from '../../services/auth/authentication.service'
 
 export class RegisterLoginComponent implements OnInit {
   thisPage: string
+  logTrue: boolean;
+  signTrue: boolean;
   constructor(private auth: AuthenticationService) { }
 
   ngOnInit() {
+    this.logTrue = true;
+    this.signTrue = false;
     this.thisPage = 'logReg';
+  }
+
+  change(){
+    this.logTrue = !this.logTrue;
+    this.signTrue = !this.signTrue;
   }
 
 }
