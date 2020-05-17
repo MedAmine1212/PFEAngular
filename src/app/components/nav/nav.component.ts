3c11a009d1ec4a45446188652abcb0157f89d9a9
ReserveHotelOnline <dr.kira1@hotmail.com>
2020-05-18 00:57:27 +0100

    Kamlna chtar l integration w l hmdlh

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -3,32 +3,6 @@ import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-nav',
-  animations: [
-    trigger(
-      'enterAnimation', [
-        transition(':enter', [
-          style({transform: 'translateX(200%)', opacity: 0}),
-          animate('500ms', style({transform: 'translateX(100%)', opacity: 1}))
-        ]),
-        transition(':leave', [
-          style({transform: 'translateX(100%)', opacity: 1}),
-          animate('500ms', style({transform: 'translateX(200%)', opacity: 0}))
-        ])
-      ]
-    ),
-    trigger(
-      'enterSecondAnimation', [
-        transition(':enter', [
-          style({transform: 'translateX(-100%)', opacity: 0}),
-          animate('500ms', style({transform: 'translateX(0%)', opacity: 1}))
-        ]),
-        transition(':leave', [
-          style({transform: 'translateX(0%)', opacity: 1}),
-          animate('500ms', style({transform: 'translateX(-100%)', opacity: 0}))
-        ])
-      ]
-    )
-  ],
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
