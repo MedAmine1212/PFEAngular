6b2adb5708a915bf2b3bb82bd10932ce56a19364
ReserveHotelOnline <dr.kira1@hotmail.com>
2020-05-25 08:47:49 +0100

    User details & edit interface

--- src/app/dialogs/employee-details/employee-details.component.ts
+++ src/app/dialogs/employee-details/employee-details.component.ts
@@ -1,6 +1,8 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {User} from '../../models/User';
+import {Department} from '../../models/Department';
+import {DepartmentService} from '../../services/departement/department.service';
 
 @Component({
   selector: 'app-employee-details',
@@ -10,14 +12,32 @@ import {User} from '../../models/User';
 export class EmployeeDetailsComponent implements OnInit {
   showUpdateForm: boolean;
   showAddressUpdateForm: boolean;
+  departments: Department[];
+  depTemp: Department = new Department();
+  i: number;
 
   constructor(
   public dialogRef: MatDialogRef<EmployeeDetailsComponent>,
-  @Inject(MAT_DIALOG_DATA) public emp: User) {}
+  @Inject(MAT_DIALOG_DATA) public emp: User, private departmentService: DepartmentService) {}
 
   ngOnInit(): void {
     this.showUpdateForm = false;
     this.showAddressUpdateForm = false;
+    this.departments = [];
+    this.departmentService.list().subscribe(r => {
+      this.departments = r;
+      this.i = 0;
+      this.depTemp = this.departments[0];
+      for (const dep of this.departments) {
+        if (dep.depId === this.emp.department.depId) {
+          this.departments[this.i] = this.depTemp;
+          break;
+        }
+        this.i++;
+      }
+      this.departments[0] = this.emp.department;
+      console.table(this.departments);
+    });
   }
 
   saveAddresses() {
@@ -26,5 +46,8 @@ export class EmployeeDetailsComponent implements OnInit {
 
   saveUser() {
 
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
