586488562ca161debd145e8390b960e98bfb00c7
ReserveHotelOnline <dr.kira1@hotmail.com>
2020-05-26 21:35:21 +0100

    Fixed JS problem

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,10 +8,27 @@ import {Component, OnInit} from '@angular/core';
 
 })
 export class LoginComponent implements OnInit {
+  private loadAPI: Promise<any>;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  reloadJs() {
+    this.loadAPI = new Promise(resolve => {
+      console.log('resolving promise...');
+      this.loadScript();
+    });
+  }
+  public loadScript() {
+    console.log('preparing to load...');
+    const node = document.createElement('script');
+    node.src = '../../../assets/scripts/temp.js';
+    node.type = 'text/javascript';
+    node.async = true;
+    node.charset = 'utf-8';
+    document.getElementsByTagName('head')[0].appendChild(node);
+  }
 }
