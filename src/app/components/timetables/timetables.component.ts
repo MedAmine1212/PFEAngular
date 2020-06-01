f1ecf92230e5520a3833088d2ad618fd62a63377
ReserveHotelOnline <dr.kira1@hotmail.com>
2020-06-01 01:08:54 +0100

    Time table

--- src/app/components/timetables/timetables.component.ts
+++ src/app/components/timetables/timetables.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Schedual} from '../../models/Schedual';
+import {Schedule} from '../../models/Schedule';
 
 
 @Component({
@@ -11,11 +11,11 @@ import {Schedual} from '../../models/Schedual';
 export class TimetablesComponent implements OnInit {
   hours: number[];
   days: string[];
-  schedual: Schedual = new Schedual();
-  schedual2: Schedual = new Schedual();
-  schedual3: Schedual = new Schedual();
-  schedual4: Schedual = new Schedual();
-  scheduals: Schedual[] = [];
+  schedule: Schedule = new Schedule();
+  schedule2: Schedule = new Schedule();
+  schedule3: Schedule = new Schedule();
+  schedule4: Schedule = new Schedule();
+  schedules: Schedule[] = [];
   time = new Date();
   showSch: boolean;
   showPause: boolean;
@@ -30,71 +30,71 @@ export class TimetablesComponent implements OnInit {
     setInterval(() => {
       this.time = new Date();
     }, 1000);
-    this.schedual.schedualId = 1;
-    this.schedual.schedualName = 'Regular schedual';
-    this.schedual.schedualDesc = 'Regular work hours schedular';
-    this.schedual.startHour = 8;
-    this.schedual.endHour = 17;
-    this.schedual.schedualDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-    this.schedual.repeadCycle = 1;
-    this.schedual.color = 'btn btn-success';
-    this.schedual.pauseTime = true;
-    this.schedual.pauseStart = 12;
-    this.schedual.pauseEnd = 14;
+    this.schedule.scheduleId = 1;
+    this.schedule.scheduleName = 'Regular schedual';
+    this.schedule.scheduleDesc = 'Regular work hours schedular';
+    this.schedule.startHour = 8;
+    this.schedule.endHour = 17;
+    this.schedule.scheduleDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+    this.schedule.repeadCycle = 1;
+    this.schedule.color = 'btn btn-success';
+    this.schedule.pauseTime = true;
+    this.schedule.pauseStart = 12;
+    this.schedule.pauseEnd = 14;
 
-    this.schedual2.schedualId = 2;
-    this.schedual2.schedualName = 'Half-Time schedual';
-    this.schedual2.schedualDesc = 'Half-time work hours schedular';
-    this.schedual2.startHour = 8;
-    this.schedual2.endHour = 13;
-    this.schedual2.schedualDays = ['Saturday'];
-    this.schedual2.repeadCycle = 1;
-    this.schedual2.color = 'btn btn-info';
-    this.schedual2.pauseTime = false;
+    this.schedule2.scheduleId = 2;
+    this.schedule2.scheduleName = 'Half-Time schedual';
+    this.schedule2.scheduleDesc = 'Half-time work hours schedular';
+    this.schedule2.startHour = 8;
+    this.schedule2.endHour = 13;
+    this.schedule2.scheduleDays = ['Saturday'];
+    this.schedule2.repeadCycle = 1;
+    this.schedule2.color = 'btn btn-info';
+    this.schedule2.pauseTime = false;
 
-    this.schedual3.schedualId = 3;
-    this.schedual3.schedualName = 'Custom schedual';
-    this.schedual3.schedualDesc = 'Custom work hours schedular';
-    this.schedual3.startHour = 8;
-    this.schedual3.endHour = 17;
-    this.schedual3.schedualDays = ['Monday', 'Wednesday', 'Friday'];
-    this.schedual3.repeadCycle = 2;
-    this.schedual3.color = 'btn btn-primary';
-    this.schedual3.pauseTime = true;
-    this.schedual3.pauseStart = 12;
-    this.schedual3.pauseEnd = 14;
+    this.schedule3.scheduleId = 3;
+    this.schedule3.scheduleName = 'Custom schedual';
+    this.schedule3.scheduleDesc = 'Custom work hours schedular';
+    this.schedule3.startHour = 8;
+    this.schedule3.endHour = 17;
+    this.schedule3.scheduleDays = ['Monday', 'Wednesday', 'Friday'];
+    this.schedule3.repeadCycle = 2;
+    this.schedule3.color = 'btn btn-primary';
+    this.schedule3.pauseTime = true;
+    this.schedule3.pauseStart = 12;
+    this.schedule3.pauseEnd = 14;
 
-    this.schedual4.schedualId = 4;
-    this.schedual4.schedualName = 'Custom2 schedual';
-    this.schedual4.schedualDesc = 'Custom2 work hours schedular';
-    this.schedual4.startHour = 8;
-    this.schedual4.endHour = 17;
-    this.schedual4.schedualDays = ['Tuesday', 'Thursday'];
-    this.schedual4.repeadCycle = 2;
-    this.schedual4.color = 'btn btn-warning';
-    this.schedual4.pauseTime = false;
+    this.schedule4.scheduleId = 4;
+    this.schedule4.scheduleName = 'Custom2 schedual';
+    this.schedule4.scheduleDesc = 'Custom2 work hours schedular';
+    this.schedule4.startHour = 8;
+    this.schedule4.endHour = 17;
+    this.schedule4.scheduleDays = ['Tuesday', 'Thursday'];
+    this.schedule4.repeadCycle = 2;
+    this.schedule4.color = 'btn btn-warning';
+    this.schedule4.pauseTime = false;
 
-    this.scheduals.push(this.schedual);
-    this.scheduals.push(this.schedual2);
-    this.scheduals.push(this.schedual3);
-    this.scheduals.push(this.schedual4);
+    this.schedules.push(this.schedule);
+    this.schedules.push(this.schedule2);
+    this.schedules.push(this.schedule3);
+    this.schedules.push(this.schedule4);
   }
 
-  setCliked(sch: Schedual, day: string, hour: number) {
-    console.log(sch.schedualName + ': ' + sch.schedualDays.indexOf(day));
+  setCliked(sch: Schedule, day: string, hour: number) {
+    console.log(sch.scheduleName + ': ' + sch.scheduleDays.indexOf(day));
     console.log(day + ' ' + hour);
   }
 
-  checkSch(sch: Schedual, day: string, hour: number) {
+  checkSch(sch: Schedule, day: string, hour: number) {
     this.showPause = false;
     this.showSch = false;
-    if ( sch.schedualDays.indexOf(day) > -1 && sch.startHour <= hour && sch.endHour >= hour) {
+    if ( sch.scheduleDays.indexOf(day) > -1 && sch.startHour <= hour && sch.endHour >= hour) {
      this.showSch = true;
    }
     if (sch.pauseTime) {
       if (sch.pauseStart <= hour && sch.pauseEnd > hour) {
         this.showSch = false;
-        if (sch.schedualDays.indexOf(day) > -1) {
+        if (sch.scheduleDays.indexOf(day) > -1) {
           this.showPause = true;
         }
       }
