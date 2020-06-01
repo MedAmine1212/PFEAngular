66773eb810cee83498e4fff4849c03e2271ca71c
ReserveHotelOnline <dr.kira1@hotmail.com>
2020-06-01 17:53:21 +0100

    Time table

--- src/app/components/timetables/timetables.component.ts
+++ src/app/components/timetables/timetables.component.ts
@@ -6,6 +6,20 @@ import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-timetables',
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({transform: 'translateX(-100%)', opacity: 0}),
+          animate('500ms', style({transform: 'translateX(0)', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateX(0)', opacity: 1}),
+          animate('0ms', style({transform: 'translateX(-100%)', opacity: 0}))
+        ])
+      ]
+    )
+  ],
   templateUrl: './timetables.component.html',
   styleUrls: ['./timetables.component.css'],
 
