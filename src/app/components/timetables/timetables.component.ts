4739d0f8ad1606d0418e8d8e61f74f29b478e44e
ReserveHotelOnline <dr.kira1@hotmail.com>
2020-06-01 01:02:05 +0100

    Time table

--- src/app/components/timetables/timetables.component.ts
+++ src/app/components/timetables/timetables.component.ts
@@ -16,12 +16,20 @@ export class TimetablesComponent implements OnInit {
   schedual3: Schedual = new Schedual();
   schedual4: Schedual = new Schedual();
   scheduals: Schedual[] = [];
+  time = new Date();
+  showSch: boolean;
+  showPause: boolean;
   constructor() {
-    this.hours = Array(25).fill(0).map((x, i) => i);
+    this.hours = Array(24).fill(0).map((x, i) => i);
     this.days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
   }
 
   ngOnInit(): void {
+    this.showPause = false;
+    this.showSch = false;
+    setInterval(() => {
+      this.time = new Date();
+    }, 1000);
     this.schedual.schedualId = 1;
     this.schedual.schedualName = 'Regular schedual';
     this.schedual.schedualDesc = 'Regular work hours schedular';
@@ -30,6 +38,9 @@ export class TimetablesComponent implements OnInit {
     this.schedual.schedualDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
     this.schedual.repeadCycle = 1;
     this.schedual.color = 'btn btn-success';
+    this.schedual.pauseTime = true;
+    this.schedual.pauseStart = 12;
+    this.schedual.pauseEnd = 14;
 
     this.schedual2.schedualId = 2;
     this.schedual2.schedualName = 'Half-Time schedual';
@@ -39,6 +50,7 @@ export class TimetablesComponent implements OnInit {
     this.schedual2.schedualDays = ['Saturday'];
     this.schedual2.repeadCycle = 1;
     this.schedual2.color = 'btn btn-info';
+    this.schedual2.pauseTime = false;
 
     this.schedual3.schedualId = 3;
     this.schedual3.schedualName = 'Custom schedual';
@@ -48,6 +60,9 @@ export class TimetablesComponent implements OnInit {
     this.schedual3.schedualDays = ['Monday', 'Wednesday', 'Friday'];
     this.schedual3.repeadCycle = 2;
     this.schedual3.color = 'btn btn-primary';
+    this.schedual3.pauseTime = true;
+    this.schedual3.pauseStart = 12;
+    this.schedual3.pauseEnd = 14;
 
     this.schedual4.schedualId = 4;
     this.schedual4.schedualName = 'Custom2 schedual';
@@ -57,6 +72,7 @@ export class TimetablesComponent implements OnInit {
     this.schedual4.schedualDays = ['Tuesday', 'Thursday'];
     this.schedual4.repeadCycle = 2;
     this.schedual4.color = 'btn btn-warning';
+    this.schedual4.pauseTime = false;
 
     this.scheduals.push(this.schedual);
     this.scheduals.push(this.schedual2);
@@ -67,5 +83,35 @@ export class TimetablesComponent implements OnInit {
   setCliked(sch: Schedual, day: string, hour: number) {
     console.log(sch.schedualName + ': ' + sch.schedualDays.indexOf(day));
     console.log(day + ' ' + hour);
+  }
+
+  checkSch(sch: Schedual, day: string, hour: number) {
+    this.showPause = false;
+    this.showSch = false;
+    if ( sch.schedualDays.indexOf(day) > -1 && sch.startHour <= hour && sch.endHour >= hour) {
+     this.showSch = true;
+   }
+    if (sch.pauseTime) {
+      if (sch.pauseStart <= hour && sch.pauseEnd > hour) {
+        this.showSch = false;
+        if (sch.schedualDays.indexOf(day) > -1) {
+          this.showPause = true;
+        }
+      }
+    }
+  }
+
+  showTip(hour: number) {
+    let h: string;
+    h = hour.toString();
+    if (hour < 10) {
+      h = '0' + h;
+    }
+    if (hour < 13) {
+      h = h + ' AM';
+    } else {
+      h = h + ' PM';
+    }
+    return h;
   }
 }
