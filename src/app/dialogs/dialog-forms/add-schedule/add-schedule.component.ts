909a7531bab2ea189f299a14b31363d709ced3b3
ReserveHotelOnline <dr.kira1@hotmail.com>
2020-06-02 12:45:13 +0100

    Add Schedule

--- src/app/dialogs/dialog-forms/add-schedule/add-schedule.component.ts
+++ src/app/dialogs/dialog-forms/add-schedule/add-schedule.component.ts
@@ -10,12 +10,17 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 export class AddScheduleComponent implements OnInit {
   formGroup: FormGroup;
   pauseTime: boolean;
+  repeatDays: string[] = [];
+  color: string;
+  iconColor: string;
 
   constructor(public dialogRef: MatDialogRef<AddScheduleComponent>,
               private formBuilder: FormBuilder,
               @Inject(MAT_DIALOG_DATA) public data: Array<any>) { }
 
   ngOnInit(): void {
+    this.color = '';
+    this.iconColor = '';
     this.pauseTime = false;
     this.FormGroup();
   }
@@ -34,5 +39,18 @@ export class AddScheduleComponent implements OnInit {
       repeatCyc: ['', [Validators.required]],
       color: ['', [Validators.required]],
     });
+  }
+
+  addRemoveDay(day: string) {
+    if (this.repeatDays.indexOf(day) > -1) {
+        this.repeatDays.splice(this.repeatDays.indexOf(day), 1);
+    } else {
+      this.repeatDays.push(day);
+    }
+  }
+
+  choseColor(color: string) {
+    this.color = 'btn btn-' + color;
+    this.iconColor = 'btn btn-outline-' + color;
   }
 }
