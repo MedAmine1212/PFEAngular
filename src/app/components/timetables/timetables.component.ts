c8e48de838d990799f417dcce63d5abfc47144e4
ReserveHotelOnline <dr.kira1@hotmail.com>
2020-06-06 05:09:12 +0100

    Sprint 1 <3

--- src/app/components/timetables/timetables.component.ts
+++ src/app/components/timetables/timetables.component.ts
@@ -18,6 +18,10 @@ export class TimetablesComponent implements OnInit {
   hours: number[];
   days: string[];
   searchText;
+  pauseStartMinutes: number;
+  pauseEndMinutes: number;
+  startMinutes: number;
+  endMinutes: number;
   showHideInput: boolean;
   plannings: Planning[] = [];
   time = new Date();
@@ -25,9 +29,21 @@ export class TimetablesComponent implements OnInit {
   showPause: boolean;
   selectedCount: number;
   desc: string;
+  startHour: number;
+  endHour: number;
+  pauseStart: number;
+  pauseEnd: number;
   constructor(public dialog: MatDialog, private  scheduleService: ScheduleService, private  planningService: PlanningService) {
     this.hours = Array(24).fill(6).map((x, i) => i);
     this.days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    this.startMinutes = 0;
+    this.endMinutes = 0;
+    this.pauseStartMinutes = 0;
+    this.pauseEndMinutes = 0;
+    this.startHour = 0;
+    this.endHour = 0;
+    this.pauseStart = 0;
+    this.pauseEnd = 0;
   }
 
   ngOnInit(): void {
@@ -50,16 +66,13 @@ export class TimetablesComponent implements OnInit {
   checkSch(pl: Planning, day: string, hour: number) {
     this.showPause = false;
     this.showSch = false;
-    const hb = pl.schedule.startHour / 60;
-    const hd = pl.schedule.endHour / 60;
-    const ps = pl.schedule.pauseStart / 60;
-    const pe = pl.schedule.pauseEnd / 60;
+    this.getTime(pl);
     if (pl.schedule.showSch) {
-      if ( pl.scheduleDays.indexOf(day) > -1 && Math.floor(hb) <= hour && Math.floor(hd) >= hour) {
+      if ( pl.scheduleDays.indexOf(day) > -1 && this.startHour <= hour && this.endHour >= hour) {
         this.showSch = true;
       }
       if (pl.schedule.pauseTime) {
-        if (Math.floor(ps) <= hour && Math.floor(pe) > hour) {
+        if (this.pauseStart <= hour && this.pauseEnd > hour) {
           this.showSch = false;
           if (pl.scheduleDays.indexOf(day) > -1) {
             this.showPause = true;
@@ -69,13 +82,20 @@ export class TimetablesComponent implements OnInit {
     }
   }
 
-  showTip(hour: number) {
+  showTip(hour: number, pl: Planning) {
     let h: string;
     h = hour.toString();
     if (hour < 10) {
       h = '0' + h;
     }
+    this.getTime(pl);
+    if ( this.startHour === hour) {
+      h = h + ':' + this.startMinutes.toString();
+    } else if (this.endHour === hour) {
+      h = h + ':' + this.endMinutes.toString();
+    } else {
     h = h + ':00';
+    }
     return h;
   }
 
@@ -134,11 +154,27 @@ export class TimetablesComponent implements OnInit {
     this.desc = '[';
     for (const day of pl.scheduleDays) {
       this.desc = this.desc + day.slice(0, 2);
-      if (pl.scheduleDays.indexOf(day) < (pl.scheduleDays.length - 1)){
+      if (pl.scheduleDays.indexOf(day) < (pl.scheduleDays.length - 1)) {
         this.desc = this.desc + ',';
       }
     }
     this.desc = this.desc + ']';
     return this.desc;
+  }
+
+  returnWidth(minutes) {
+    return Math.floor((minutes * 100 ) / 60 )  + '% !important';
+  }
+
+  private getTime(pl) {
+    this.startHour = Math.floor(pl.schedule.startHour / 60);
+    this.startMinutes = pl.schedule.startHour % 60;
+    this.endHour = Math.floor(pl.schedule.endHour / 60);
+    this.endMinutes = pl.schedule.endHour % 60;
+    this.pauseStart = Math.floor(pl.schedule.pauseStart / 60);
+    this.pauseStartMinutes = pl.schedule.pauseStart % 60;
+    this.pauseEnd = Math.floor(pl.schedule.pauseEnd / 60);
+    this.pauseEndMinutes = pl.schedule.pauseEnd % 60;
+
   }
 }
